9fa8f49e917aa8ed56a8d4f02a2da9173e1012bd
nekashtanka <trofimovacarinka@yandex.ru>
2025-02-13 09:51:42 +0800

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Karina on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizTests/QuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizTests.swift
+//  QuizTests
+//
+//  Created by Karina on 13.02.2025.
+//
+
+import Testing
+@testable import Quiz
+
+struct QuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
