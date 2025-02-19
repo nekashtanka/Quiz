1c1972ae6e6a3a4a7efc782d096268185e9799bc
nekashtanka <trofimovacarinka@yandex.ru>
2025-02-19 14:03:23 +0800

    в

--- Quiz/BNRAppDelegate.swift
+++ Quiz/BNRAppDelegate.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
-class BNRAppDelegate: NSObject {
+@UIApplicationMain
+class BNRAppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
 }
